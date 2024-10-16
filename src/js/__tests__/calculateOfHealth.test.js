973de96c9b66172b0432e2ce00bd79270d026d82
Andrey Maximov <arman.gen40@mail.ru>
2024-10-16 21:23:25 +0300

    Change tests

--- src/js/__tests__/calculateOfHealth.test.js
+++ src/js/__tests__/calculateOfHealth.test.js
@@ -1,20 +1,12 @@
 import calculateOfHealth from '../calculateOfHealth';
 
 const arr = [
-  {name: 'Маг', health: 90, message: 'healthy'},
-  {name: 'Shark', health: 23, message: 'wounded'},
-  {name: 'Viki', health: 11, message: 'critical'},
+  [{name: 'Маг', health: 90, message: 'healthy'}],
+  [{name: 'Shark', health: 23, message: 'wounded'}],
+  [{name: 'Viki', health: 11, message: 'critical'}],
 ]
 
-test('show health check', () => {
-  const result = calculateOfHealth(arr[0]);
-  expect(result).toBe(arr[0].message);
-})
-test('show health check', () => {
-  const result = calculateOfHealth(arr[1]);
-  expect(result).toBe(arr[1].message);
-})
-test('show health check', () => {
-  const result = calculateOfHealth(arr[2]);
-  expect(result).toBe(arr[2].message);
+test.each(arr)('show health check', (obj) => {
+  const result = calculateOfHealth(obj);
+  expect(result).toBe(obj.message)
 })
--- src/js/calculateOfHealth.js
+++ src/js/calculateOfHealth.js
@@ -13,11 +13,11 @@ const players = {
 export default function calculateOfHealth (obj) {
     if (obj.health > 50) {
       return players.healthy.message;
-    };
+    }
     if (obj.health >= 15 & obj.health <= 50) {
       return players.wounded.message;
-    };
+    }
     if (obj.health < 15) {
       return players.critical.message;
-    };
+    }
 }
